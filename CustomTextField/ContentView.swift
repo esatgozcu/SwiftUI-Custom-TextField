a2d5d1740e072246e6775afe166d032909dc9771
Esat GÖZCÜ <esatgozcu@gmail.com>
2023-03-30 09:55:29 +1000

    Initial Project

---
+++ CustomTextField/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CustomTextField
+//
+//  Created by Esat Gözcü on 2023/03/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomTextField/CustomTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTextFieldApp.swift
+//  CustomTextField
+//
+//  Created by Esat Gözcü on 2023/03/30.
+//
+
+import SwiftUI
+
+@main
+struct CustomTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
